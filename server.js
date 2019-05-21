5d11456aa33359474b0dee0a2df1ff1b70266e75
RomanSlep <slep48@gmail.com>
2019-05-21 16:33:50 +0300

    add api server

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 
 /**
- * @description http watched DB tables
- */
+* @description http watched DB tables
+*/
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('./modules/DB');
-const port = 36669;
+const config = require('./modules/configReader');
+const port = config.api;
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-
-app.get('/db', (req, res) => {
-    const tb = db[req.query.tb];
-    if (!tb){
-        res.json({err: 'tb not find'});
-        return;
-    }
-    tb.find({}, (err, docs) => res.json(docs || {err}));
-});
-
-app.listen(port, () => console.info('Server listening on port ' + port + ' http://localhost:' + port));
+if (port) {
+	app.use(bodyParser.json()); // for parsing application/json
+	app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+	
+	app.get('/db', (req, res) => {
+		const tb = db[req.query.tb];
+		if (!tb){
+			res.json({err: 'tb not find'});
+			return;
+		}
+		tb.find({}, (err, docs) => res.json(docs || {err}));
+	});
+	
+	app.listen(port, () => console.info('Server listening on port ' + port + ' http://localhost:' + port));
+	
+}	
